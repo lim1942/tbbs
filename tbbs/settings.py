044e913fd86225c3724fee7fc2d7d9c27b717fcf
Your Name <you@example.com>
2021-07-28 18:13:42 +0800

    中间件“

--- tbbs/settings.py
+++ tbbs/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -143,3 +144,5 @@ REST_FRAMEWORK = {
 # 跨越
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+
+APPEND_SLASH=False
